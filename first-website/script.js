700084cd8f4887d2a925f571ce46889d540a83c6
timinis <litimc3@gmail.com>
2019-11-20 18:54:22 -0800

    add linkedlist sample

--- first-website/script.js
+++ first-website/script.js
@@ -1,8 +1,11 @@
 const groceryList = [];
 const emptyArea = document.getElementById('list-area');
+const warningArea = document.getElementById('warning');
 
 function rerender() {
   emptyArea.innerHTML = '';
+  warningArea.innerHTML = '';
+
   for (let i = 0; i < groceryList.length; i++) {
     const listContainer = document.createElement('div');
     const listItem = document.createElement('p');
@@ -24,6 +27,10 @@ function submitInput(event) {
 
   const userInput = inputNode.value;
   const name = fieldName.value;
+  if (userInput.length === 0 || name.length === 0) {
+    rerender();
+    return generateEmptyWarning();
+  }
   groceryList.push(userInput);
   groceryList.push(name);
   inputNode.value = '';
@@ -35,6 +42,14 @@ const button = document.getElementById('submitbutton');
 button.onclick = submitInput;
 
 //Button is missing a click event
+
+function generateEmptyWarning() {
+  event.preventDefault();
+  console.log(warningArea);
+  const warning = document.createElement('p');
+  warning.textContent = 'empty inputs please fill out';
+  warningArea.appendChild(warning);
+}
 
 function deleteItem(event) {
   event.preventDefault();
